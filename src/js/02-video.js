611b315ef96563a7e14833b810e4acff725dcbf0
pavliv182 <uptn182@gmail.com>
2022-07-25 15:26:29 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,14 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-const key = 'videoplayer-current-time';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 // console.log(player);
 
 function playerCurrentTime(evt) {
-  localStorage.setItem(key, evt.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, evt.seconds);
   // console.log(evt.seconds);
 }
 
-player.setCurrentTime(Number(localStorage.getItem(key)));
+player.setCurrentTime(Number(localStorage.getItem(CURRENT_TIME_KEY)));
 player.on('timeupdate', throttle(playerCurrentTime, 1000));
 // test/
