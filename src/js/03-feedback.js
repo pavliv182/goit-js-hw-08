e055fb2215701d4f69061e334a8080292c29aadf
pavliv182 <uptn182@gmail.com>
2022-07-26 18:21:05 +0300

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,50 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-// console.log(formEl);
 
 const FORM_KEY = 'feedback-form-state';
 
-const formData = {};
-
-function formFill(form) {
-  const elements = form.elements;
-  //   console.dir(elements);
-
-  const localStorageData = JSON.parse(localStorage.getItem(FORM_KEY));
-
-  if (!localStorageData) {
-    return;
-  }
-
-  const keys = Object.keys(localStorageData);
-  keys.forEach(el => {
-    elements[el].value = localStorageData[el];
-    // console.log(el);
-  });
-}
-// console.log(formEl.elements);
 function localStorageUpdateTime(evt) {
-  //   console.log(Date.now());
+  const formData = JSON.parse(localStorage.getItem(FORM_KEY)) || {};
 
-  const name = evt.target.name;
-
-  const value = evt.target.value;
-
-  formData[name] = value;
-
+  formData[evt.target.name] = evt.target.value;
+  // записываем данные, которые ввели в инпут в localstorage (если ввели что либо)
   localStorage.setItem(FORM_KEY, JSON.stringify(formData));
 }
+
 formEl.addEventListener('input', throttle(localStorageUpdateTime, 500));
 
 formEl.addEventListener('submit', evt => {
   evt.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
   localStorage.removeItem(FORM_KEY);
   formEl.reset();
 });
 
+// Эта функция заполняет поля формы данными из localstorage, после того, как мы перезагружаем страницу
+
+function formFill(form) {
+  const elements = form.elements;
+
+  const localStorageData = JSON.parse(localStorage.getItem(FORM_KEY));
+  // Если нет данных в localstorage то прекращаем выполнение функции
+  if (!localStorageData) {
+    return;
+  }
+  // Если данные есть, то записываем данные в форму.
+  const keys = Object.keys(localStorageData);
+  keys.forEach(el => {
+    elements[el].value = localStorageData[el];
+    // console.log(localStorageData[el]);
+  });
+}
+
 formFill(formEl);
+
+// formData[name] = value;
+// - это
+// form.email = input.value
+// form.message = textarea.value
+// formData = {
+//   email: ukefhfdl(то, что мы вводим),
+//   message: fiddfjil(то, что мы вводим),
+// };
